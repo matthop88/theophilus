67aa27631a70d0fcb7d4d8b3a14622e6b80a68fa
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 10:31:58 -0800

    Updated outer main to use love.load to determine args

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
-require("app/main")
-
-require("test/framework/application")
+function love.load(args)
+    if args[1] == "test" then require("test/framework/application")
+    else                      require("app/main")
+    end
+end
