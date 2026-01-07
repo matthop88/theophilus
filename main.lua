3d5ebb2a7e95deaaaeb5621e7357302a8a1500e7
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 10:28:01 -0800

    Updated outer main to require test application

--- main.lua
+++ main.lua
@@ -1,15 +1,3 @@
 require("app/main")
 
-local DISCOVER_TEST_CLASSES   = require("test/framework/discovery/discoverTestClasses")
-local DISCOVER_TEST_FUNCTIONS = require("test/framework/discovery/discoverTestFunctions")
-
-local testClasses = DISCOVER_TEST_CLASSES()
-
-for n, testClass in ipairs(testClasses) do
-    print(n .. ": " .. testClass:getName())
-
-    local testFunctions = DISCOVER_TEST_FUNCTIONS(testClass)
-    for k, v in pairs(testFunctions) do
-        v(testClass)
-    end 
-end
+require("test/framework/application")
