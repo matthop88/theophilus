146b1595d83e882b1571ffcbaabbf22fa2393f75
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 10:41:10 -0800

    Updated test framework application to be more readable

--- test/framework/application.lua
+++ test/framework/application.lua
@@ -1,10 +1,16 @@
 local DISCOVER_TEST_CLASSES   = require("test/framework/discovery/discoverTestClasses")
 local DISCOVER_TEST_FUNCTIONS = require("test/framework/discovery/discoverTestFunctions")
 
+local printCaption = function(str)
+    print(str .. "\n" .. string.rep("-", string.len(str)))
+end
+
 local testClasses = DISCOVER_TEST_CLASSES()
 
+print("\n")
+
 for n, testClass in ipairs(testClasses) do
-    print(n .. ": " .. testClass:getName())
+    printCaption(testClass:getName())
 
     local testFunctions = DISCOVER_TEST_FUNCTIONS(testClass)
     for testName, testFn in pairs(testFunctions) do
