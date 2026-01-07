ce4bfdf35e2e45845b103410231dc0570246c5b4
Matthew Hopkins <matthop88@gmail.com>
2026-01-06 16:52:30 -0800

    Update assertEquals.lua; removed self from functions

--- test/framework/assert/assertEquals.lua
+++ test/framework/assert/assertEquals.lua
@@ -1,11 +1,11 @@
 local ASTERISKS = require("test/framework/const/asterisks")
 
-local passedResult = function(self, name)
+local passedResult = function(name)
 	print("PASSED => " .. name)
 	return true
 end
 
-local failedResult = function(self, name, actual, expected)
+local failedResult = function(name, actual, expected)
 	print("\n" .. ASTERISKS .. "FAILED => " .. name)
     print("  Expected: ", expected)
     print("  Actual: ",   actual)
@@ -13,7 +13,7 @@ local failedResult = function(self, name, actual, expected)
     return false
 end
 
-return function(self, name, actual, expected)
+return function(name, actual, expected)
 	if expected == actual then
 		return passedResult(name)
 	else
