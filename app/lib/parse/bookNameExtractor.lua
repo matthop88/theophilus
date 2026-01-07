2ffbde14648a035aadc3d452905da15ecc2ad03b
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 10:58:31 -0800

    Updated bookNameExtractor.lua to handle error case for 0 arguments

--- app/lib/parse/bookNameExtractor.lua
+++ app/lib/parse/bookNameExtractor.lua
@@ -2,16 +2,21 @@ return {
     execute = function(self, params)
         local args = params.args
 
-        local bookName, passage
+        if args == nil or #args == 0 then
+            return { error = "INSUFFICIENT ARGUMENTS: No Arguments Given" }
 
-        if tonumber(args[1]) then
-            bookName = args[1] .. " " .. args[2]
-            passage  = args[3]
         else
-            bookName = args[1]
-            passage  = args[2]
-        end
+            local bookName, passage
 
-        return { book = bookName, passage = passage }
+            if tonumber(args[1]) then
+                bookName = args[1] .. " " .. args[2]
+                passage  = args[3]
+            else
+                bookName = args[1]
+                passage  = args[2]
+            end
+
+            return { book = bookName, passage = passage }
+        end
     end,
 }
