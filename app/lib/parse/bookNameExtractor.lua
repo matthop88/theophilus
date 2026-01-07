6c372247634cd979dacb40924165c7c8d6f4947f
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 10:46:10 -0800

    Updated bookNameExtractor.lua for happy path. Throws exception

--- app/lib/parse/bookNameExtractor.lua
+++ app/lib/parse/bookNameExtractor.lua
@@ -2,6 +2,11 @@ return {
     execute = function(self, params)
         local args = params.args
 
-        return { book = "SomeBookName", passage = "x:y-x:z" }
+        local bookName, passage
+
+        bookName = args[1]
+        passage  = args[2]
+
+        return { book = args[1], passage = args[2] }
     end,
 }
