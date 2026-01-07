1b6c1cd18f75c902f7dd9f4d76c2d667ad7ae80d
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 11:09:05 -0800

    Updated bookNameExtractor.lua by refactoring

--- app/lib/parse/bookNameExtractor.lua
+++ app/lib/parse/bookNameExtractor.lua
@@ -1,19 +1,29 @@
+local insufficientArgsError = function(value)
+    if value == nil then
+        return { error = "INSUFFICIENT ARGUMENTS: No Arguments Given" }
+    else
+        return { error = "INSUFFICIENT ARGUMENTS: '" .. value .. "' -> Book Name Expected" }
+    end
+end
+
+local bookPassageResult = function(book, passage)
+    return { book = book, passage = passage }
+end
+
+local parseArgs = function(args)
+    if tonumber(args[1]) then
+        if args[2] == nil then return insufficientArgsError(args[1])
+        else                   return bookPassageResult(args[1] .. " " .. args[2], args[3])   end
+    else
+        return bookPassageResult(args[1], args[2])
+    end
+end
+
 return {
     execute = function(self, params)
         local args = params.args
 
-        if args == nil or #args == 0 then
-            return { error = "INSUFFICIENT ARGUMENTS: No Arguments Given" }
-        else
-            if tonumber(args[1]) then
-                if args[2] == nil then
-                    return { error = "INSUFFICIENT ARGUMENTS: '" .. args[1] .. "' -> Book Name Expected" }
-                else
-                    return { book = args[1] .. " " .. args[2], passage = args[3] }
-                end
-            else
-                return { book = args[1], passage = args[2] }
-            end
-        end
+        if args == nil or #args == 0 then return insufficientArgsError()
+        else                              return parseArgs(args)     end
     end,
 }
