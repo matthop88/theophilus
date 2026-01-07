9c3e92640c73013c57643395a5df885040b0e934
Matthew Hopkins <matthop88@gmail.com>
2026-01-06 16:25:05 -0800

    Create testBookNameExtractor.lua

---
+++ test/tests/testBookNameExtractor.lua
@@ -0,0 +1,69 @@
+local BOOK_NAME_EXTRACTOR = require("app/lib/parse/bookNameExtractor")
+
+return {
+    getName = function(self) 
+        return "Book Name Extractor Tests"
+    end,
+
+    beforeAll = function(self)
+        -- Do nothing
+    end,
+
+    before = function(self)
+        -- Do nothing
+    end,
+
+    runExtractor = function(self, params)
+        local result = BOOK_NAME_EXTRACTOR:execute(params)
+
+        if     result.error then
+            return "error = " .. result.error
+        elseif result.passage then
+            return "book = " .. result.book .. ", passage = " .. result.passage
+        else
+            return "book = " .. result.book
+        end
+    end,
+    
+    testSingleArgumentBookName = function(self)
+        local name = "Single Argument Book Name, Happy Path"
+
+        local result = self:runExtractor { args = { "Genesis", "1:1" } }
+        
+        return TESTING:assertEquals(name, result, "book = Genesis, passage = 1:1")
+    end,
+
+    testTwoArgumentBookName = function(self)
+        local name = "Two Argument Book Name, Happy Path"
+
+        local result = self:runExtractor { args = { "1", "John", "3:16" } }
+
+        return TESTING:assertEquals(name, result, "book = 1 John, passage = 3:16")
+    end,
+
+    testNoPassage = function(self)
+        local name = "No Passage, Happy Path"
+
+        local result = self:runExtractor { args = { "Philippians" } }
+
+        return TESTING:assertEquals(name, result, "book = Philippians")
+    end,
+
+    testZeroArgumentBookNameError = function(self)
+        local name = "Zero Argument Book Name, Error Path"
+
+        local result = self:runExtractor { args = {} }
+
+        return TESTING:assertEquals(name, result, "error = INSUFFICIENT ARGUMENTS: No Arguments Given")
+    end,
+
+    testSingleArgumentBookNameError = function(self)
+        local name = "SingleArgument Book Name, Error Path"
+
+        local result = self:runExtractor { args = { "1" } }
+
+        return TESTING:assertEquals(name, result, error = "INSUFFICIENT ARGUMENTS: '1' -> Book Name Expected")
+    end,
+}
+            
+        
