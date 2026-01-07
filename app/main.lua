51402721479e108b318dadbd02f068d883bb511b
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 11:19:33 -0800

    Updated inner main to parse args and print out result from book name extractor

--- app/main.lua
+++ app/main.lua
@@ -1,3 +1,8 @@
-print("Welcome to Theophilus!")
+local BOOK_NAME_EXTRACTOR = require("app/lib/parse/bookNameExtractor")
+local displayTable        = require("app/lib/display/displayTable")
+
+local result = BOOK_NAME_EXTRACTOR:execute { args = __ARGS }
+
+displayTable(result)
 
 love.event.quit()
