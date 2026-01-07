91e694c9950c4a18404089484ae58c11a4b7b360
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 11:18:54 -0800

    Updated outer main to pass args to inner main

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 function love.load(args)
-    if args[1] == "test" then require("test/framework/application")
-    else                      require("app/main")
+    if args[1] == "test" then 
+        require("test/framework/application")
+    else        
+        __ARGS = args              
+        require("app/main")
     end
 end
