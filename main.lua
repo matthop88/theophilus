a24cf01e221e95b0f20e99c5c77eda8e8aba4299
Matthew Hopkins <matthop88@gmail.com>
2026-01-06 16:55:22 -0800

    Updated main.lua to test out discoverTests.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
-require "app/main"
+require("app/main")
+
+local DISCOVER_TESTS = require("test/framework/discovery/discoverTests")
+
+local tests = DISCOVER_TESTS()
+
+for n, test in ipairs(tests) do
+    print(n .. ": " .. test:getName())
+end
