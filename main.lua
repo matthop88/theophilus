991d9659be256c04c03ea8082d60b6d2d0e18db6
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 10:19:42 -0800

    Updated main.lua to utilize discoverTestFunctions

--- main.lua
+++ main.lua
@@ -1,9 +1,15 @@
 require("app/main")
 
-local DISCOVER_TESTS = require("test/framework/discovery/discoverTestClasses")
+local DISCOVER_TEST_CLASSES   = require("test/framework/discovery/discoverTestClasses")
+local DISCOVER_TEST_FUNCTIONS = require("test/framework/discovery/discoverTestFunctions")
 
-local tests = DISCOVER_TESTS()
+local testClasses = DISCOVER_TEST_CLASSES()
 
-for n, test in ipairs(tests) do
-    print(n .. ": " .. test:getName())
+for n, testClass in ipairs(testClasses) do
+    print(n .. ": " .. testClass:getName())
+
+    local testFunctions = DISCOVER_TEST_FUNCTIONS(testClass)
+    for k, v in pairs(testFunctions) do
+        print("  " .. k)
+    end 
 end
