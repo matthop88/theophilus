0a0825f44d2617af76df7974ab0895ff5e36f8d2
Matthew Hopkins <matthop88@gmail.com>
2026-01-07 10:27:36 -0800

    Extracted testing code to test/framework/application.lua

---
+++ test/framework/application.lua
@@ -0,0 +1,13 @@
+local DISCOVER_TEST_CLASSES   = require("test/framework/discovery/discoverTestClasses")
+local DISCOVER_TEST_FUNCTIONS = require("test/framework/discovery/discoverTestFunctions")
+
+local testClasses = DISCOVER_TEST_CLASSES()
+
+for n, testClass in ipairs(testClasses) do
+    print(n .. ": " .. testClass:getName())
+
+    local testFunctions = DISCOVER_TEST_FUNCTIONS(testClass)
+    for testName, testFn in pairs(testFunctions) do
+        testFn(testClass)
+    end 
+end
