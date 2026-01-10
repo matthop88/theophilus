b6a8c750fe13e5b64c985084d8b72ccdf97ee82d
Matthew Hopkins <matthop88@gmail.com>
2026-01-09 16:37:11 -0800

    Update lookup.lua; made it real

--- app/lib/lookup/util/lookup.lua
+++ app/lib/lookup/util/lookup.lua
@@ -1,17 +1,19 @@
-local lookupFunction = function(directory, files)
+local STRING_UTIL = require("app/lib/util/stringUtil")
+
+local lookupFunction
+
+lookupFunction = function(directory, files)
     files = files or {}
     
     local directoryItems = love.filesystem.getDirectoryItems(directory)
 
     for _, v in ipairs(directoryItems) do
-        --[[
-            local path = directory .. "/" .. v
-            if v ends with ".lua" then
-                table.insert(files, path)
-            else 
-                lookupFunction(path, files)
-            end      
-        ]]
+        local path = directory .. "/" .. v
+        if STRING_UTIL:endsWith(v, ".lua") then
+            table.insert(files, path)
+        else 
+            lookupFunction(path, files)
+        end      
     end
 
     return files
