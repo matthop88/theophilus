64939c05b5ab92834a3bf2b5a6b0426e500a61e5
Matthew Hopkins <matthop88@gmail.com>
2026-01-13 10:14:34 -0800

    Created bookLookup.lua

---
+++ app/lib/lookup/bookLookup.lua
@@ -0,0 +1,25 @@
+local LOOKUP_FILES  = require("app/lib/lookup/util/lookup")
+
+return {
+	execute = function(self, params)
+		local dataObjs = LOOKUP_FILES("data", function(data) return data.book == params.book end)
+
+		if #dataObjs == 0 then
+			return {
+				book = params.book, bookData = nil, error = "BOOK NOT FOUND: " .. params.book
+			}
+		else
+			local bookData = dataObjs[1]
+			if params.version then
+				for _, dataObj in ipairs(dataObjs) do
+					if dataObj.version == params.version then bookData = dataObj end
+				end
+			end
+
+			return {
+				book = bookData.book, bookData = bookData, version = bookData.version
+			}
+		end
+	end,
+
+}
