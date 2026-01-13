87bf08cb3d472676cb0e7612045e76f73604e704
Matthew Hopkins <matthop88@gmail.com>
2026-01-13 10:15:19 -0800

    Created testBookLookup.lua

---
+++ test/tests/testBookLookup.lua
@@ -0,0 +1,29 @@
+local BOOK_LOOKUP  = require("app/lib/lookup/bookLookup")
+local ASSERT_EQUALS = require("test/framework/assert/assertEquals")
+
+return {
+    getName = function(self) 
+        return "Book Lookup tests"
+    end,
+
+    beforeAll = function(self)
+        -- Do nothing
+    end,
+
+    before = function(self)
+        -- Do nothing
+    end,
+
+    testLookupBookNameSuccess = function(self)
+        local name = "Book Lookup by name, Happy Path"
+
+        local result = BOOK_LOOKUP:execute { book = "Philippians" }
+        
+        local bookDataString = "nil"
+        if result.bookData then bookDataString = "(DATA)" end
+        local resultString = "{ book = " .. result.book .. ", bookData = " .. bookDataString .. ", version = " .. result.version .. " }"
+        return ASSERT_EQUALS(name, resultString, "{ book = Philippians, bookData = (DATA), version = KJV }")
+    end,
+}
+            
+        
