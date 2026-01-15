1fae986edab90319a5e8678fee2eb1b16ca05ff5
Matthew Hopkins <matthop88@gmail.com>
2026-01-15 08:34:08 -0800

    Create bookMetaLookup.lua

---
+++ app/lib/lookup/bookMetaLookup.lua
@@ -0,0 +1,32 @@
+local BOOK_LOOKUP = require("app/lib/lookup/bookLookup")
+
+--[[
+
+1. Takes a parameter list consisting of a book name, passage and optional version.
+2. Uses bookLookup to look up based upon book name and version.
+3. If book isn't found, response is:
+   { book = "Genesis", bookData = nil, error = { BOOK_NOT_FOUND { book = "Genesis" } }, }
+4. If book is found, response is:
+   { book = "Genesis", bookData = (data object), version = (versionFound), chapterCount = 50, passage = "1:1," }
+5. If version isn't found, response is the same, but the following field is added:
+   warning = VERSION_NOT_FOUND { book = "Genesis", requested = "NIV", found = "NASB 95" },
+
+]]
+
+return {
+	execute = function(self, params)
+		if params.error then 
+			return params
+		else
+			local result = BOOK_LOOKUP:execute(params)
+			if result.error then 
+				return result
+			else
+				result.chapterCount = result.bookData.chapterCount
+				result.passage      = params.passage
+
+				return result
+			end
+		end
+	end,
+}
