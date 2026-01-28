6fae83e84beeafa835cd2117f1de6a3e07ed1ea8
Matthew Hopkins <matthop88@gmail.com>
2026-01-28 10:40:46 -0800

    Updated inner main to do a meta lookup on the specified book.

--- app/main.lua
+++ app/main.lua
@@ -1,8 +1,10 @@
 local BOOK_NAME_EXTRACTOR = require("app/lib/parse/bookNameExtractor")
+local BOOK_META_LOOKUP    = require("app/lib/lookup/bookMetaLookup")
 local displayTable        = require("app/lib/display/displayTable")
 
-local result = BOOK_NAME_EXTRACTOR:execute { args = __ARGS }
+local parseResult  = BOOK_NAME_EXTRACTOR:execute { args = __ARGS }
+local lookupResult = BOOK_META_LOOKUP:execute(parseResult)
 
-displayTable(result)
+displayTable(lookupResult)
 
 love.event.quit()
