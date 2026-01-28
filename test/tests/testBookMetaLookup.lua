a6af334c6de055bafdc689c8407ef7c640c464c3
Matthew Hopkins <matthop88@gmail.com>
2026-01-28 10:26:55 -0800

    Updated testBookMetaLookup.lua; added preexisting error test

--- test/tests/testBookMetaLookup.lua
+++ test/tests/testBookMetaLookup.lua
@@ -18,9 +18,10 @@ return {
         local passageString = result.passage or "nil"
         local bookDataString = "nil"
         if result.bookData then bookDataString = "(DATA)" end
+        local bookString    = "" .. (result.book or "nil")
         local versionString = "" .. (result.version or "nil")
         local chapterCountString = "" .. (result.chapterCount or "nil")
-        local resultString = "{ book = " .. result.book .. ", bookData = " .. bookDataString .. ", version = " .. versionString .. ", chapterCount = " .. chapterCountString .. ", passage = " .. passageString
+        local resultString = "{ book = " .. bookString .. ", bookData = " .. bookDataString .. ", version = " .. versionString .. ", chapterCount = " .. chapterCountString .. ", passage = " .. passageString
         
         if result.warning then resultString = resultString .. ", warning = " .. result.warning end
         if result.error   then resultString = resultString .. ", error = "   .. result.error   end
@@ -68,5 +69,12 @@ return {
         return ASSERT_EQUALS(name, resultString, "{ book = Hesitations, bookData = nil, version = nil, chapterCount = nil, passage = 3:16, error = BOOK NOT FOUND: Hesitations }")
     end,
 
+    testLookupBookMetaPreexistingErrorFailure = function(self)
+        local name = "Book Meta Lookup with preexisting error, Unhappy Path"
+
+        local result = BOOK_META_LOOKUP:execute { error = "INSUFFICIENT ARGUMENTS: No Arguments Given" }
+        local resultString = self:resultToString(result)
+        return ASSERT_EQUALS(name, resultString, "{ book = nil, bookData = nil, version = nil, chapterCount = nil, passage = nil, error = INSUFFICIENT ARGUMENTS: No Arguments Given }")
+    end,
 
 }
