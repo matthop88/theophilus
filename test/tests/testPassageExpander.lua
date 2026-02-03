a4bbdfb0ea5ab18247f7530287107821c142c3b3
Matthew Hopkins <matthop88@gmail.com>
2026-02-02 16:48:43 -0800

    Updated testPassageExpander.lua; added multiple verse test

--- test/tests/testPassageExpander.lua
+++ test/tests/testPassageExpander.lua
@@ -149,4 +149,12 @@ return {
         return ASSERT_EQUALS(name, resultString, "{ book = John, chapterCount = 21, passage = 3:16Z, error = INVALID VALUE in passage: 3:16Z }")
     end,
 
+    testExpansionChapterAndMultipleVerses = function(self)
+        local name = "Passage Expansion for Chapter and Multiple Verses, Happy Path"
+
+        local result = PASSAGE_EXPANDER:execute { book = "Ephesians", passage = "2:8-9", chapterCount = 6 }
+        local resultString = self:resultToString(result)
+        return ASSERT_EQUALS(name, resultString, "{ book = Ephesians, chapterCount = 6, passage = 2:8-9, body = { { chapter = 2, verse = 8-9 }, } }")
+    end,
+
 }
