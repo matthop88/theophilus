be30d50b8d978b5c6c80e5930e34dca727fe3145
Matthew Hopkins <matthop88@gmail.com>
2026-02-09 12:50:50 -0800

    Added wildcard test to testVerseExpander.lua

--- test/tests/testVerseExpander.lua
+++ test/tests/testVerseExpander.lua
@@ -69,4 +69,45 @@ return {
         return ASSERT_EQUALS(name, resultString, "{ book = Ephesians, chapterCount = 6, passage = 2:8-9, body = { { chapter = 2, verse = 8-9, warning = Ephesians Chapter 2 is missing from the dataset }, } }")
     end,
 
+    testExpansionSingleWildcardSuccess = function(self)
+        local name = "Verse Expansion with Single Wildcard, Happy Path"
+
+        local data = {
+            body = {
+                {
+                    chapter = 1,
+                    verse = "18-?",
+                },
+                {  
+                    chapter = 2,
+                    verse = "1-9",
+                },
+            },
+            book = "Ephesians",
+            passage = "1:18-2:9",
+            chapterCount = 6,
+            bookData = {
+                book = "Ephesians",
+                chapterCount = 6,
+                version      = "NASB 95",
+                chapters     = {   
+                    {   
+                        chapter    = 1,
+                        verseCount = 23,
+                        verses     = { },
+                    },
+                    {  
+                        chapter    = 2,
+                        verseCount = 23,
+                        verses     = { },
+                    },
+                },
+            },
+        }
+
+        local result = VERSE_EXPANDER:execute(data)
+        local resultString = self:resultToString(result)
+        return ASSERT_EQUALS(name, resultString, "{ book = Ephesians, chapterCount = 6, passage = 1:18-2:9, body = { { chapter = 1, verse = 18-23 }, { chapter= 2, verse = 1-9 }, } }")
+    end,
+
 }
