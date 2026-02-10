02d3c70575ddd5d58f4ee8c7bfbe75fc7aee946b
Matthew Hopkins <matthop88@gmail.com>
2026-02-09 16:16:10 -0800

    Updated verseExpander.lua

--- app/lib/parse/verseExpander.lua
+++ app/lib/parse/verseExpander.lua
@@ -16,6 +16,9 @@ return {
 		if params.error then
 			return params
 		else
+			local outOfRangeVerses = {}
+			local result = params
+
 			for _, elt in ipairs(params.body) do
 				local foundChapter = false
 				for _, c in ipairs(params.bookData.chapters) do
@@ -24,13 +27,30 @@ return {
 						if STRING_UTIL:endsWith(elt.verse, "?") then
 							elt.verse = string.sub(elt.verse, 1, string.len(elt.verse) - 1) .. c.verseCount
 						end
+						local verses = STRING_UTIL:split(elt.verse, "-")
+						local verseNum1 = tonumber(verses[1])
+						local verseNum2 = tonumber(verses[2]) or verseNum1
+						local maxVerseNum = math.max(verseNum1, verseNum2)
+
+						if maxVerseNum > c.verseCount then
+							local diff = maxVerseNum - c.verseCount
+							if diff == 1 then
+								table.insert(outOfRangeVerses, params.book .. " " .. elt.chapter .. ":" .. maxVerseNum)
+							else
+								table.insert(outOfRangeVerses, params.book .. " " .. elt.chapter .. ":" .. (c.verseCount + 1) .. "-" .. maxVerseNum)
+							end
+						end
 					end
 				end
 				if not foundChapter then
 					elt.warning = params.book .. " Chapter " .. elt.chapter .. " is missing from the dataset"
 				end
 			end
-			local result = params
+
+			if #outOfRangeVerses > 0 then
+				result.error = "The following verses are out of range: " .. STRING_UTIL:join(outOfRangeVerses, ", ")
+			end
+			
 			return result
 		end
 	end,
