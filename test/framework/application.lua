dc10d1d354f0a44498ec59b7af6c133116a1a37b
Matthew Hopkins <matthop88@gmail.com>
2026-02-10 11:57:21 -0800

    Updated application.lua to properly specify name of failing test

--- test/framework/application.lua
+++ test/framework/application.lua
@@ -14,21 +14,34 @@ for n, testClass in ipairs(testClasses) do
 
     local testFunctions = DISCOVER_TEST_FUNCTIONS(testClass)
     local testCount, successfulTests = 0, 0
+    local failedTests = {}
     for testName, testFn in pairs(testFunctions) do
         testCount = testCount + 1
-        local status, err = pcall(function() testFn(testClass) end)
-        if status == true then
-            successfulTests = successfulTests + 1
-        else
+        local testResult = nil
+        local status, err = pcall(function() testResult = testFn(testClass) end)
+        if status ~= true then
+            table.insert(failedTests, testName)
             print()
             print("FAILED => " .. testName)
             print("          WITH ERROR: " .. err)
             print()
         end
+        if testResult then 
+            if testResult.succeeded then successfulTests = successfulTests + 1
+            else                         table.insert(failedTests, testResult.name)  end
+        end
     end 
 
     print()
     print("Tests succeeded: " .. successfulTests .. " out of " .. testCount)
+    if #failedTests > 0 then
+        print()
+        print("The following tests have failed:")
+        print("--------------------------------")
+        for n, t in ipairs(failedTests) do
+            print(n .. ". " .. t)
+        end
+    end
 end
 
 print()
