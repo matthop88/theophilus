9e7a1e35ede46f1afb530ff43e9f18267b9249f0
Matthew Hopkins <matthop88@gmail.com>
2026-02-13 11:44:10 -0800

    Added code in verse lookup to pass along chapter warning messages

--- app/lib/lookup/verseLookup.lua
+++ app/lib/lookup/verseLookup.lua
@@ -2,6 +2,22 @@ return {
 	execute = function(self, params)
 		if params.error then return params end
 		
+		local result = {}
+
+		for _, requestPart in ipairs(params.request) do
+			local resultPart = {
+				chapter = requestPart.chapter,
+				warning = requestPart.warning,
+			}
+
+			if not requestPart.warning then
+				-- look up verse
+			end
+
+			table.insert(result, resultPart)
+		end
+
+		params.result = result
 		return params
 	end,
 
