098a37fb18238c61dbc5fb3307d68ae351790393
Matthew Hopkins <matthop88@gmail.com>
2026-02-17 10:58:12 -0800

    Updated inner main to utilize verse lookup code

--- app/main.lua
+++ app/main.lua
@@ -2,6 +2,7 @@ local BOOK_NAME_EXTRACTOR = require("app/lib/parse/bookNameExtractor")
 local BOOK_META_LOOKUP    = require("app/lib/lookup/bookMetaLookup")
 local PASSAGE_EXPANDER    = require("app/lib/parse/passageExpander")
 local VERSE_EXPANDER      = require("app/lib/parse/verseExpander")
+local VERSE_LOOKUP        = require("app/lib/lookup/verseLookup")
 
 local displayTable        = require("app/lib/display/displayTable")
 
@@ -9,8 +10,9 @@ local parseResult         = BOOK_NAME_EXTRACTOR:execute { args = __ARGS }
 local lookupResult        = BOOK_META_LOOKUP:execute(parseResult)
 local expandPassageResult = PASSAGE_EXPANDER:execute(lookupResult)
 local expandVerseResult   = VERSE_EXPANDER:execute(expandPassageResult)
+local lookupVerseResult   = VERSE_LOOKUP:execute(expandVerseResult)
 
-local result = expandVerseResult
+local result = lookupVerseResult
 if result.bookData then result.bookData = "(BOOK DATA)" end
 
 displayTable(result)
