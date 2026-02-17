14759f18dc1475604ddfd6f27e66724c2d06c5bc
Matthew Hopkins <matthop88@gmail.com>
2026-02-17 10:53:36 -0800

    Added test for missing verses

--- test/tests/testVerseLookup.lua
+++ test/tests/testVerseLookup.lua
@@ -21,6 +21,33 @@ local EPHESIANS = {
     },
 }
 
+local PHILIPPIANS = {
+    book         = "Philippians",
+    chapterCount = 4,
+    version      = "NASB 95",
+    chapters     = {
+        {   chapter    = 1,
+            verseCount = 30,
+            verses = {
+                {   verse = 1,
+                    "Paul and Timothy, bond-servants of Christ Jesus,",
+                    "To all the saints in Christ Jesus who are in Philippi, including the overseers and deacons:",
+                },
+                {   verse = 2,
+                    "Grace to you and peace from God our Father and the Lord Jesus Christ.",
+                },
+                {   verse = 6,
+                    "For I am confident of this very thing,",
+                    "that He who began a good work in you will perfect it until the day of Christ Jesus.",
+                },
+                {   verse = 7,
+                    "For it is only right for me to feel this way about you all, because I have you in my heart,",
+                    "since both in my imprisonment and in the defense and confirmation of the gospel, you all are partakers of grace with me.",
+                },
+            },
+        },
+    },
+}
 return {
     getName = function(self) 
         return "Verse Lookup tests"
@@ -59,13 +86,17 @@ return {
         local firstResult = lookupResult[1]
         local resultString = "{ { chapter = " .. firstResult.chapter .. ", verseCount = " .. firstResult.verseCount .. ", "
         for _, verse in ipairs(firstResult) do
-            resultString = resultString .. "{ verse = " .. verse.verse .. ", "
-            for _, fragment in ipairs(verse) do
-                resultString = resultString .. "\"" .. fragment .. "\", "
+            if verse.warning then
+                resultString = resultString .. "{ warning = " .. verse.warning .. " "
+            else
+                resultString = resultString .. "{ verse = " .. verse.verse .. ", "
+                for _, fragment in ipairs(verse) do
+                    resultString = resultString .. "\"" .. fragment .. "\", "
+                end
             end
             resultString = resultString .. "}, "
         end
-        return resultString .. " } }"
+        return resultString .. "} }"
     end,
 
     testLookupVersePropagateErrorFailure = function(self)
@@ -90,6 +121,22 @@ return {
 
         local result = VERSE_LOOKUP:execute(data)
         local resultString = self:resultToString(result)
-        return ASSERT_EQUALS(name, resultString, "{ book = Ephesians, chapterCount = 6, passage = 1:22-23, request = { { chapter = 1, verse = 22-23 }, result = { { chapter = 1, verseCount = 23, { verse = 22, \"And He put all things in subjection under His feet,\", \"and gave Him as head over all things to the church,\", }, { verse = 23, \"which is His body, the fullness of Him who fills all in all.\", },  } } }")
+        return ASSERT_EQUALS(name, resultString, "{ book = Ephesians, chapterCount = 6, passage = 1:22-23, request = { { chapter = 1, verse = 22-23 }, result = { { chapter = 1, verseCount = 23, { verse = 22, \"And He put all things in subjection under His feet,\", \"and gave Him as head over all things to the church,\", }, { verse = 23, \"which is His body, the fullness of Him who fills all in all.\", }, } } }")
+    end,
+
+    testLookupMissingVersesHappyPath = function(self)
+        local name = "Missing Verse Lookup, Happy Path"
+
+        local data = {
+            book = "Philippians",
+            passage = "1:1-9",
+            chapterCount = 4,
+            bookData = PHILIPPIANS,
+            request = { { chapter = 1, verse = "1-9", }, },
+        }
+
+        local result = VERSE_LOOKUP:execute(data)
+        local resultString = self:resultToString(result)
+        return ASSERT_EQUALS(name, resultString, "{ book = Philippians, chapterCount = 4, passage = 1:1-9, request = { { chapter = 1, verse = 1-9 }, result = { { chapter = 1, verseCount = 30, { verse = 1, \"Paul and Timothy, bond-servants of Christ Jesus,\", \"To all the saints in Christ Jesus who are in Philippi, including the overseers and deacons:\", }, { verse = 2, \"Grace to you and peace from God our Father and the Lord Jesus Christ.\", }, { warning = The following verses are missing: Philippians 1:3-5 }, { verse = 6, \"For I am confident of this very thing,\", \"that He who began a good work in you will perfect it until the day of Christ Jesus.\", }, { verse = 7, \"For it is only right for me to feel this way about you all, because I have you in my heart,\", \"since both in my imprisonment and in the defense and confirmation of the gospel, you all are partakers of grace with me.\", }, { warning = The following verses are missing: Philippians 1:8-9 }, } } }")
     end,
 }
