ec5915dd6cf199366d3efce5257c0b34964b23da
Matthew Hopkins <matthop88@gmail.com>
2026-02-17 10:53:05 -0800

    Updated verseLookup.lua to add warnings for missing verses

--- app/lib/lookup/verseLookup.lua
+++ app/lib/lookup/verseLookup.lua
@@ -3,6 +3,35 @@ local STRING_UTIL = require("app/lib/util/stringUtil")
 local lookupChapter = require("app/lib/lookup/util/lookupChapter")
 local lookupVerse   = require("app/lib/lookup/util/lookupVerse")
 
+local MISSING_VERSES = {
+	fromVerse = nil,
+	toVerse   = nil,
+
+	add = function(self, verseNum)
+		if self.fromVerse == nil then
+			self.fromVerse = verseNum
+		end
+		self.toVerse = verseNum
+	end,
+
+	hasData = function(self)
+		return self.fromVerse ~= nil
+	end,
+
+	clear = function(self)
+		self.fromVerse = nil
+		self.toVerse   = nil
+	end,
+
+	toString = function(self, bookName, chapterNum)
+		if self.fromVerse == self.toVerse then
+			return "The following verse is missing: " .. bookName .. " " .. chapterNum .. ":" .. self.fromVerse
+		else
+			return "The following verses are missing: " .. bookName .. " " .. chapterNum .. ":" .. self.fromVerse .. "-" .. self.toVerse
+		end
+	end, 
+}
+
 return {
 	execute = function(self, params)
 		if params.error then return params end
@@ -24,17 +53,31 @@ return {
 				local chapterData = lookupChapter(params.bookData, requestPart.chapter)
 				resultPart.verseCount = chapterData.verseCount
 				
+				MISSING_VERSES:clear()
+
 				for verseNum = tonumber(fromVerse), tonumber(toVerse) do
 					local dstVersePart = {
 						verse = verseNum,
 					}
 
 					local verseData   = lookupVerse(chapterData, verseNum)
-					for _, srcVersePart in ipairs(verseData) do
-						table.insert(dstVersePart, srcVersePart)
+					if verseData == nil then
+						MISSING_VERSES:add(verseNum)
+					else
+						if MISSING_VERSES:hasData() then
+							table.insert(resultPart, { warning = MISSING_VERSES:toString(params.book, requestPart.chapter) })
+							MISSING_VERSES:clear()
+						end
+						for _, srcVersePart in ipairs(verseData) do
+							table.insert(dstVersePart, srcVersePart)
+						end
+						table.insert(resultPart, dstVersePart)
 					end
+				end
 
-					table.insert(resultPart, dstVersePart)
+				if MISSING_VERSES:hasData() then
+					table.insert(resultPart, { warning = MISSING_VERSES:toString(params.book, requestPart.chapter) })
+					MISSING_VERSES:clear()
 				end
 
 			end
