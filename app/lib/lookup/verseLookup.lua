c65f6ddeae145cef5b37c6f127cc9be5c798ca19
Matthew Hopkins <matthop88@gmail.com>
2026-02-17 10:29:56 -0800

    Updated verseLookup.lua so it actually looks up verses

--- app/lib/lookup/verseLookup.lua
+++ app/lib/lookup/verseLookup.lua
@@ -1,3 +1,8 @@
+local STRING_UTIL = require("app/lib/util/stringUtil")
+
+local lookupChapter = require("app/lib/lookup/util/lookupChapter")
+local lookupVerse   = require("app/lib/lookup/util/lookupVerse")
+
 return {
 	execute = function(self, params)
 		if params.error then return params end
@@ -12,6 +17,26 @@ return {
 
 			if not requestPart.warning then
 				-- look up verse
+				local verseRange = STRING_UTIL:split(requestPart.verse, "-")
+				local fromVerse = verseRange[1]
+				local toVerse   = verseRange[2] or fromVerse
+
+				local chapterData = lookupChapter(params.bookData, requestPart.chapter)
+				resultPart.verseCount = chapterData.verseCount
+				
+				for verseNum = tonumber(fromVerse), tonumber(toVerse) do
+					local dstVersePart = {
+						verse = verseNum,
+					}
+
+					local verseData   = lookupVerse(chapterData, verseNum)
+					for _, srcVersePart in ipairs(verseData) do
+						table.insert(dstVersePart, srcVersePart)
+					end
+
+					table.insert(resultPart, dstVersePart)
+				end
+
 			end
 
 			table.insert(result, resultPart)
