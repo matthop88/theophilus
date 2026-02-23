8624f44eef7fc4574b8095a9e64e9ca4da8b83bd
Matthew Hopkins <matthop88@gmail.com>
2026-02-23 12:36:31 -0800

    Now print out warnings for missing chapters

--- app/main.lua
+++ app/main.lua
@@ -60,22 +60,26 @@ else
 	for _, ch in ipairs(result.result) do
 		displayCaption(result.book .. " " .. ch.chapter)
 
-		for _, v in ipairs(ch) do
-			if v.warning then
-				print("\nWARNING: " .. v.warning .. "\n")
-			else
-				for n, s in ipairs(v) do
-					local ss = s
-					if   STRING_UTIL:endsWith(s, ".")
-					  or STRING_UTIL:endsWith(s, "?")
-					  or STRING_UTIL:endsWith(s, "!")
-					  or STRING_UTIL:endsWith(s, ":")
-					then
-						ss = ss .. "\n"
-					end
+		if ch.warning then
+			print("\nWARNING: " .. ch.warning .. "\n")
+		else
+			for _, v in ipairs(ch) do
+				if v.warning then
+					print("\nWARNING: " .. v.warning .. "\n")
+				else
+					for n, s in ipairs(v) do
+						local ss = s
+						if   STRING_UTIL:endsWith(s, ".")
+						  or STRING_UTIL:endsWith(s, "?")
+						  or STRING_UTIL:endsWith(s, "!")
+						  or STRING_UTIL:endsWith(s, ":")
+						then
+							ss = ss .. "\n"
+						end
 
-					if n == 1 then print(v.verse .. " " .. ss)
-					else print(ss)                         end
+						if n == 1 then print(v.verse .. " " .. ss)
+						else print(ss)                         end
+					end
 				end
 			end
 		end
