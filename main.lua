21d65da4cf0c21acc4068dda83200d8be1ba1103
Matthew Hopkins <matthop88@gmail.com>
2026-02-24 13:02:13 -0800

    Updated main.lua to allow "BIBLE" as 1st arg

--- main.lua
+++ main.lua
@@ -1,7 +1,14 @@
 function love.load(args)
     if args[1] == "test" then 
         require("test/framework/application")
-    else        
+    elseif args[1] and string.upper(args[1]) == "BIBLE" then
+        __ARGS = {}
+        for n, arg in ipairs(args) do
+            if n > 1 then table.insert(__ARGS, arg) end
+        end
+
+        require("app/bible/bibleApp")        
+    else
         __ARGS = args              
         require("app/main")
     end
