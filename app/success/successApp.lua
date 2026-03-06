cabc293e131ed2c14328c9cc8575e393473c76d9
Matthew Hopkins <matthop88@gmail.com>
2026-03-06 13:26:47 -0800

    Create successApp.lua

---
+++ app/success/successApp.lua
@@ -0,0 +1,14 @@
+local TRACKING_DATA = require("app/lib/scheduling/trackingData")
+
+local data = TRACKING_DATA.load()
+
+if not data then
+    print("\nSUCCESS: No tracking data found. Run SCHEDULE first.\n")
+else
+    local selected = TRACKING_DATA.select(data)
+    selected.nextDate = os.time() + 60
+    TRACKING_DATA.save(data)
+    print("\nSUCCESS recorded for: " .. selected.reference .. "\n")
+end
+
+love.event.quit()
