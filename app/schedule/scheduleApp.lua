62463a92640969a848b21eac4923bcddafd7889f
Matthew Hopkins <matthop88@gmail.com>
2026-03-06 12:11:14 -0800

    Updated scheduleApp.lua to write trackingData file

--- app/schedule/scheduleApp.lua
+++ app/schedule/scheduleApp.lua
@@ -1,8 +1,15 @@
-local STRING_UTIL = require("app/lib/util/stringUtil")
+local STRING_UTIL    = require("app/lib/util/stringUtil")
+local TRACKING_DATA  = require("app/lib/scheduling/trackingData")
 
 local scheduleList = require("data/scheduling/scheduleList")
 
-local firstEntry = scheduleList[1]
-__ARGS = STRING_UTIL:split(firstEntry, " ")
+local data = TRACKING_DATA.load()
+if not data then
+    data = TRACKING_DATA.generate(scheduleList)
+end
+
+local selected = TRACKING_DATA.select(data)
+
+__ARGS = STRING_UTIL:split(selected.reference, " ")
 
 require("app/bible/bibleApp")
