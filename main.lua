c5e0b7c553c41c030c120a9c0113e1fda7c52e1f
Matthew Hopkins <matthop88@gmail.com>
2026-03-06 15:39:18 -0800

    Updated outer main.lua to pass parameters to success app

--- main.lua
+++ main.lua
@@ -10,15 +10,22 @@ function love.load(args)
         end
         require("app/cloak/cloakApp")
     elseif args[1] and string.upper(args[1]) == "SUCCESS" then
+        __ARGS = {}
+        for n, arg in ipairs(args) do
+            if n > 1 then table.insert(__ARGS, arg) end
+        end
         require("app/success/successApp")
     elseif args[1] and string.upper(args[1]) == "FAIL" then
+        __ARGS = {}
+        for n, arg in ipairs(args) do
+            if n > 1 then table.insert(__ARGS, arg) end
+        end
         require("app/fail/failApp")
     elseif args[1] and string.upper(args[1]) == "BIBLE" then
         __ARGS = {}
         for n, arg in ipairs(args) do
             if n > 1 then table.insert(__ARGS, arg) end
         end
-
         require("app/bible/bibleApp")        
     else
         __ARGS = args              
